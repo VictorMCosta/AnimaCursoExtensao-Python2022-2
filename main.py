dd990a5dfbb56fae43923bd66181a9674b1139e5
Victor Mendes Amaral Costa <103154005+VictorMCosta@users.noreply.github.com>
2022-10-20 01:31:27 +0000

    Aprendi a declarar variável

---
+++ main.py
@@ -0,0 +1,16 @@
+# Meu primeiro Projeto Python!!!
+# print() = comando de saida
+print("Alo Mundo!")
+
+# Quando quiser guardar uma String!
+nome = "Victor Mendes"
+# Quando quiser guardar um numero inteiro!
+idade = 18
+# Exibir o nome
+print("Seu nome é ", nome)
+print(f"A minha idade é: {idade} ")
+
+
+'''
+comentario em bloco
+'''
