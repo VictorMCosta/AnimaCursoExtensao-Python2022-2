ff7a65ef15a01e4795701753c40396d1b8c392bd
Victor Mendes Amaral Costa <103154005+VictorMCosta@users.noreply.github.com>
2022-11-17 01:12:52 +0000

    aula4c.py

---
+++ aula4d.py
@@ -0,0 +1,23 @@
+import aula4c.py as vm
+
+con, cur = vm.conectar()
+
+nome = input("Informe o nome do herói/vilão")
+nome_civil = input("Informe o nome civil do herói/vilão (sua indentidade secreta): ")
+tipo_numerico = input("Tecle 1 para Herói (na) ou 2 para Vilão(o)")
+
+#Consulta para o valor máximo usado no banco
+sql = "SELECT MAX(pessoa_id)+1 FROM pessoas"
+cur.execute(sql)
+pessoa_id = cur.fetchone()[0]
+
+if tipo_numerico == "1"
+  tipo = "Herói (na)"
+else:
+  tipo = "Vilão(o)"
+
+sql = f"INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES ({pessoa_id}, '{nome}', '{nome_civil}', '{tipo}')"
+
+cur.execute(sql)
+con.commit()
+con.colse()
